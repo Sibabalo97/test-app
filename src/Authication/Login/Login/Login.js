42a9285aa4ca8d37bc58746dc500a0eec829c3cf
Sibabamlo@LSIBABALOM <Sibabalo.Maqiyane@redpandasoftware.co.za>
2024-01-22 17:03:05 +0200

    changes

---
+++ src/Authication/Login/Login/Login.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+import './Login.css';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    
+    navigate('/Dashboard');
+  };
+  
+
+  return (
+    <div className="login-container">
+      <h1>Login</h1>
+      <button className="google-button">Continue with Google</button>
+
+      <form>
+        <label>Username</label>
+        <input
+          type="text"
+          placeholder="Enter your username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label>Password</label>
+        <div className="password-input">
+          <input
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div className="remember-me">
+          <input type="checkbox" id="remember" />
+          <label htmlFor="remember">Remember me</label>
+        </div>
+
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+        {error && <p className="error-message">{error}</p>}
+      </form>
+
+      <p>
+        Don't have an account? <Link to="/register">Sign up</Link>
+      </p>
+    </div>
+  );
+}
+
+export default Login;
